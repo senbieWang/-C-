9f4a39b76f19605e2549a5bae44aebddc9045f05
senbeiuser <john_doe@xx.com>
2020-10-16 19:25:16 +0800

    virtual copy constructor

--- EffictiveCppLearn/Techniques/term25.cpp
+++ EffictiveCppLearn/Techniques/term25.cpp
@@ -10,10 +10,21 @@ NewLetter::NewLetter(std::istream& str)
 	}
 }
 
+
+NewLetter::NewLetter(const NewLetter& rhs)
+{
+	for (auto it = rhs.components.begin(); it != rhs.components.end(); ++it) 
+	{
+		components.push_back((*it)->clone());
+	}
+}
+
+
 //产生一个 textblock  或者  graphic 对象，具体由读入的 str来决定，
 //由于产生新对象，所以行为放入  constructor,由于能产生不同类型的对象， 可以视为 virtual constructor.
 NLComponent* NewLetter::readComponent(std::istream& str) 
 {
 	//if str == graph  new Griphic
 	//if str == textblock  new TextBlock
+	return nullptr;
 }
--- EffictiveCppLearn/Techniques/term25.h
+++ EffictiveCppLearn/Techniques/term25.h
@@ -4,19 +4,29 @@ class NLComponent
 {
 public:
 	virtual void  fun() = 0;
+	virtual NLComponent* clone() const = 0;
 };
 
 //文字新闻
 class TextBlock :public NLComponent  
 {
 public:
-
+	void  fun() {}
+	virtual TextBlock* clone() const  //virtual  copy constructor
+	{
+		return new TextBlock(*this);
+	}
 };
 
 //图片新闻
 class Griphic : public NLComponent 
 {
 public:
+	void  fun() {}
+	virtual Griphic* clone() const
+	{
+		return new Griphic(*this);
+	}
 
 };
 
@@ -25,8 +35,9 @@ class NewLetter
 {
 public:
 	NewLetter(std::istream& str);  //从stream中读取新闻
+	NewLetter(const NewLetter& rhs);
 private:
-	static NLComponent* readComponent(std::istream& str);
+	static NLComponent* readComponent(std::istream& str);  //virtual constructor 
 private:
 	std::list<NLComponent*>  components;
 };
